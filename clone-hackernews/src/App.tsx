9c4f2bde55448c1e5f889744c0f574866b69f370
Rutvik2901 <gajjarrutvik291@gmail.com>
2021-09-07 18:37:46 +0530

    app.tsx

--- clone-hackernews/src/App.tsx
+++ clone-hackernews/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "App.css";
 import CommentComponent from "Components/CommentComponent";
