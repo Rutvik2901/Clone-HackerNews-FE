5bb31027b2611eb652510a66e52a9e9b775534da
Rutvik2901 <gajjarrutvik291@gmail.com>
2021-09-07 19:18:51 +0530

    TSCONFIG

--- clone-hackernews/src/App.tsx
+++ clone-hackernews/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "App.css";
 import CommentComponent from "Components/CommentComponent";
@@ -8,11 +8,10 @@ import HomeComponent from "Components/HomeComponent";
 function App() {
   return (
     <div className="App">
-      <HomeComponent />
-      {/* <BrowserRouter> */}
-      {/* <Route exact path="/" component={HomeComponent} /> */}
-      {/* <Route exact path="/comment/post/:id" render={(props) => <CommentComponent {...props} id={props.match.params.id} />} /> */}
-      {/* </BrowserRouter> */}
+      <BrowserRouter>
+        <Route exact path="/" component={HomeComponent} />
+        <Route exact path="/comment/post/:id" render={(props) => <CommentComponent {...props} id={props.match.params.id} />} />
+      </BrowserRouter>
     </div>
   );
 }
